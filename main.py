2d367addc1f9caf9a8fa99fba96055bfb6336612
ryyan <ryyan@pm.me>
2021-04-24 21:17:54 -0500

    Update server to serve web page

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import socket
 import time
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
 
 from data import load
 
@@ -10,21 +10,28 @@ host = "0"
 port = 8000
 
 
-class Server(BaseHTTPRequestHandler):
+class RequestHandler(SimpleHTTPRequestHandler):
+    valid_paths = [
+        "/index.html",
+        "/results.json",
+    ]
+
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "application/json")
-        self.send_header("Access-Control-Allow-Origin", "*")
+        if self.path == "/":
+            self.path = "/index.html"
+
+        if self.path in RequestHandler.valid_paths:
+            return SimpleHTTPRequestHandler.do_GET(self)
+
+        self.send_response(404)
         self.end_headers()
-        self.wfile.write(DataLoader.data.encode("utf_8"))
 
 
 def main():
     with open("results.json", "w") as outfile:
         outfile.write(json.dumps(load(), indent=2))
 
-    return
-    server = HTTPServer((host, port), Server)
+    server = ThreadingHTTPServer((host, port), RequestHandler)
 
     try:
         print(f"Server started at {host}:{port}")
